1542041684b74f136e8a679858350644e12ec384
lk235 <cool.lk235@cloud.com>
2017-04-10 21:08:35 +0800

    Create Uri Contract

--- app/src/main/java/com/example/android/myappportfolio/topMovies/data/MovieContract.java
+++ app/src/main/java/com/example/android/myappportfolio/topMovies/data/MovieContract.java
@@ -1,5 +1,8 @@
 package com.example.android.myappportfolio.topMovies.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,13 +12,35 @@ import android.provider.BaseColumns;
 
 public class MovieContract {
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.myappportfolio.topMovies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_MOVIE = "movie";
+
     public static final class MovieEntry implements BaseColumns{
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.ANY_CURSOR_ITEM_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
         public static final String TABLE_NAME = "movie";
         public static final String COLUMN_IMAGE_URL = "imageUrl";
         public static final String COLUMN_TITLE = "title";
         public static final String COLUMN_RELEASE_DATE = "release_date";
         public static final String COLUMN_VOTE = "vote";
         public static final String COLUMN_OVER_VIEW = "overview";
+
+ 
+
+        public static Uri buildMovieByCategory(String category){
+            return CONTENT_URI.buildUpon().appendPath(category).build();
+        }
     }
+
+
 
 }
